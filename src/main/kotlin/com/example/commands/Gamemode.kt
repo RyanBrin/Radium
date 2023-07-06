cf025d5fea8884ad87213166dd40d9def9d4fd73
RyanBrin <ryanjbrinkman@outlook.com>
2023-07-06 16:01:04 -0500

    text additions
    added text in chat when you switch gamemodes

--- src/main/kotlin/com/example/commands/Gamemode.kt
+++ src/main/kotlin/com/example/commands/Gamemode.kt
@@ -2,6 +2,9 @@ package com.example.commands
 import co.aikar.commands.BaseCommand
 import co.aikar.commands.annotation.CommandAlias
 import co.aikar.commands.annotation.CommandPermission
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.NamedTextColor
+import net.kyori.adventure.text.format.TextColor
 import org.bukkit.GameMode
 import org.bukkit.entity.Player
 
@@ -9,25 +12,33 @@ object Gamemode : BaseCommand() {
 
     @CommandAlias("gms")
     @CommandPermission("radium.gamemode.gms")
-    fun gms(player: Player) {
-        player.gameMode = GameMode.SURVIVAL
+    fun gms(sender: Player) {
+        sender.gameMode = GameMode.SURVIVAL
+        sender.sendMessage(Component.text("Your game mode has been updated to Survival Mode")
+            .color(TextColor.fromHexString("#0aaaff")))
     }
 
     @CommandAlias("gma")
     @CommandPermission("radium.gamemode.gma")
-    fun gma(player: Player) {
-        player.gameMode = GameMode.ADVENTURE
+    fun gma(sender: Player) {
+        sender.gameMode = GameMode.ADVENTURE
+        sender.sendMessage(Component.text("Your game mode has been updated to Adventure Mode")
+            .color(TextColor.fromHexString("#0aaaff")))
     }
 
     @CommandAlias("gmc")
     @CommandPermission("radium.gamemode.gmc")
-    fun gmc(player: Player) {
-        player.gameMode = GameMode.CREATIVE
+    fun gmc(sender: Player) {
+        sender.gameMode = GameMode.CREATIVE
+        sender.sendMessage(Component.text("Your game mode has been updated to Creative Mode")
+            .color(TextColor.fromHexString("#0aaaff")))
     }
 
     @CommandAlias("gmsp")
     @CommandPermission("radium.gamemode.gmsp")
-    fun gmsp(player: Player) {
-        player.gameMode = GameMode.SPECTATOR
+    fun gmsp(sender: Player ) {
+        sender.gameMode = GameMode.SPECTATOR
+        sender.sendMessage(Component.text("Your game mode has been updated to Spectator Mode")
+            .color(TextColor.fromHexString("#0aaaff")))
     }
 }
